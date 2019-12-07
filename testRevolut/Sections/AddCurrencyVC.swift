5bdf4e48c44c0002c7b2e7ec904a7c0df7ea95f9
Pavle Mijatović <paul.mijatovic@sprintingsoftware.com>
2019-12-07 13:41:13 +0100

    5

--- testRevolut/Sections/AddCurrencyVC.swift
+++ testRevolut/Sections/AddCurrencyVC.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class AddCurrencyVC: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var addCurrencyBtn: AddBtn!
+    @IBOutlet weak var addCurrencyTxtBtn: UIButton!
+    @IBOutlet weak var addCurrencyInfoLbl: UILabel!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func addCurrency(_ sender: Any) {
+        print("Add.....")
     }
-    */
+    
+    // MARK: - Helper
 
 }
